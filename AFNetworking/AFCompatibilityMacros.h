ade3e323536e1fcdb986f803b791ccc7473cc82d
maChao <1325563868@qq.com>
2020-10-30 16:10:26 +0800

    修改

--- AFNetworking/AFCompatibilityMacros.h
+++ AFNetworking/AFCompatibilityMacros.h
@@ -1,6 +1,6 @@
 // AFCompatibilityMacros.h
 // Copyright (c) 2011–2016 Alamofire Software Foundation ( http://alamofire.org/ )
-//
+//哈哈哈哈哈哈哈哈哈哈红红火火恍恍惚惚
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
